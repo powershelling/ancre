54dffdfc8e2c7b667586bd18d3e83d563fc0207d
powershelling <46499561+powershelling@users.noreply.github.com>
2023-03-05 23:40:37 +0100

    ancre
    Create script

---
+++ ancre/client/client.lua
@@ -0,0 +1,29 @@
+local anchored = false
+local boat = nil
+Citizen.CreateThread(function()
+	while true do
+
+		Wait(0)
+		local ped = GetPlayerPed(-1)
+		if IsPedInAnyBoat(ped) then
+		boat  = GetVehiclePedIsIn(ped, true)
+		end
+		if IsControlJustPressed(0, 47) and not IsPedInAnyVehicle(ped) and boat ~= nil then
+			if not anchored then
+				SetBoatAnchor(boat, true)
+				TaskStartScenarioInPlace(ped, 'CODE_HUMAN_MEDIC_TEND_TO_DEAD', 0, true)
+				Citizen.Wait(10000))
+				ClearPedTasks(ped)
+			else
+				TaskStartScenarioInPlace(ped, 'CODE_HUMAN_MEDIC_TEND_TO_DEAD', 0, true)
+				Citizen.Wait(10000)
+				SetBoatAnchor(boat, false)
+				ClearPedTasks(ped)
+			end
+			anchored = not anchored
+		end
+				if IsVehicleEngineOn(boat) then
+			anchored = false
+		end
+	end
+end)
---
+++ ancre/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author ' Powershelling'
+description 'Possbility to freeze a boat on water'
+
+
+client_script {
+	"client/client.lua"
+}
