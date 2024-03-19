bb14143b07a78856e421e113e0b20aaa36b8bd1a
powershelling <46499561+powershelling@users.noreply.github.com>
2024-03-19 09:04:07 +0100

    Update client.lua

--- ancre/client/client.lua
+++ ancre/client/client.lua
@@ -1,40 +1,44 @@
 local anchored = false
 local boat = nil
 local ESX = nil
+local ped = nil
 
 Citizen.CreateThread(function()
     while ESX == nil do
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
         Citizen.Wait(0)
     end
-end)
 
-Citizen.CreateThread(function()
     while true do
-        Wait(0)
-        local ped = GetPlayerPed(-1)
+        Citizen.Wait(0)
+        ped = GetPlayerPed(-1)
+
         if IsPedInAnyBoat(ped) then
             boat = GetVehiclePedIsIn(ped, true)
-        end
-        local coords = GetEntityCoords(ped)
-        local distanceToBoat = boat and GetDistanceBetweenCoords(coords, GetEntityCoords(boat), true) or 999
-        if distanceToBoat < 3.0 and not IsPedInAnyVehicle(ped) then
-            ESX.ShowHelpNotification("Appuyez sur ~INPUT_CONTEXT~ pour jeter/lâcher l'ancre.")
-            if IsControlJustPressed(0, 47) and boat ~= nil then
-                if not anchored then
-                    SetBoatAnchor(boat, true)
-                    TaskStartScenarioInPlace(ped, 'CODE_HUMAN_MEDIC_TEND_TO_DEAD', 0, true)
-                    Citizen.Wait(10000)
-                    ClearPedTasks(ped)
-                else
-                    TaskStartScenarioInPlace(ped, 'CODE_HUMAN_MEDIC_TEND_TO_DEAD', 0, true)
-                    Citizen.Wait(10000)
-                    SetBoatAnchor(boat, false)
-                    ClearPedTasks(ped)
-                end
-                anchored = not anchored
+        elseif boat ~= nil then
+            if GetDistanceBetweenCoords(GetEntityCoords(ped), GetEntityCoords(boat), true) > 3.0 or not DoesEntityExist(boat) then
+                boat = nil
             end
         end
+
+        if boat ~= nil and not IsPedInAnyVehicle(ped) then
+            ESX.ShowHelpNotification("Appuyez sur ~INPUT_CONTEXT~ pour jeter/lâcher l'ancre.")
+            if IsControlJustPressed(0, 47) then
+                TaskStartScenarioInPlace(ped, 'CODE_HUMAN_MEDIC_TEND_TO_DEAD', 0, true)
+                Citizen.Wait(1000)
+
+                if not anchored then
+                    SetBoatAnchor(boat, true)
+                else
+                    SetBoatAnchor(boat, false)
+                end
+
+                anchored = not anchored
+                Citizen.Wait(9000)
+                ClearPedTasks(ped)
+            end
+        end
+
         if IsVehicleEngineOn(boat) then
             anchored = false
         end
